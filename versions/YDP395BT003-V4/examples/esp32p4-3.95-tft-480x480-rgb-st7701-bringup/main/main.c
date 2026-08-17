6adb7854006495feb0f5bddfffcd34d7e34be0b0
BLSNCD <2857585364@qq.com>
2026-08-17 09:38:03 +0800

    docs(examples): add OSPTEK SPDX header to example entry main.c

--- versions/YDP395BT003-V4/examples/esp32p4-3.95-tft-480x480-rgb-st7701-bringup/main/main.c
+++ versions/YDP395BT003-V4/examples/esp32p4-3.95-tft-480x480-rgb-st7701-bringup/main/main.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 OSPTEK
+ * SPDX-License-Identifier: CC-BY-4.0
+ *
+ * https://github.com/osptek
+ */
+
 #include "app_init.h"
 #include "lv_demos.h"
 #include "esp_log.h"
